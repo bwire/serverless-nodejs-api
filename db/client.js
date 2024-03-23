bf391099d002ff6029f47e4107294d8c471e320f
barbedwire <e.barbedwire@gmail.com>
2024-03-23 20:23:04 +0100

    add module refactoring

---
+++ db/client.js
@@ -0,0 +1,9 @@
+const { neon } = require('@neondatabase/serverless');
+const { getDatabaseUrl } = require('../lib/secrets');
+
+async function getDbClient() {
+  const dbUrl = await getDatabaseUrl();
+  return neon(dbUrl);
+}
+
+module.exports.getDbClient = getDbClient;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,19 @@
 const serverless = require('serverless-http');
 const express = require('express');
-const { neon, neonConfig } = require('@neondatabase/serverless');
-const AWS = require('aws-sdk');
+const { getDbClient } = require('../db/client');
 
 const app = express();
 
-const AWS_REGION = 'us-east-1';
-const STAGE = process.env.STAGE || 'prod';
-const DB_URL_SSM_PARAM = `/serverless-nodejs-api/${STAGE}/database-url`;
-
-const ssm = new AWS.SSM({ region: AWS_REGION });
-
-async function dbClient() {
-  const paramStored = await ssm
-    .getParameter({
-      Name: DB_URL_SSM_PARAM,
-      WithDecryption: true,
-    })
-    .promise();
-
-  return neon(paramStored.Parameter.Value);
-}
-
 app.get('/', async (req, res, next) => {
-  const sql = await dbClient();
-  const [result] = await sql`select now();`;
+  const sql = await getDbClient();
+  const now = Date.now();
+
+  const [dbNowResult] = await sql`select now();`;
+  const delta = (dbNowResult.now.getTime() - now) / 1000;
+
   return res.status(200).json({
     message: 'Hello from root!',
-    result: result.now,
+    delta,
   });
 });
 
@@ -42,9 +28,5 @@ app.use((req, res, next) => {
     error: 'Not Found',
   });
 });
-
-// app.listen(3000, () => {
-//   console.log('Server is running at port 3000');
-// });
 
 module.exports.handler = serverless(app);
